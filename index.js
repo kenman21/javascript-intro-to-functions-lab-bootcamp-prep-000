204e92ec1631c2008d3f7d2183fdbdd76bc23f88
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2017-12-21 04:15:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function say
+function sayHiToGrandma(string) {
+  if string.toLowerCase == string
+  return "I can't hear you!"
+  else if string.toUpperCase == string
+  return "YES INDEED!"
+  else if string = "I love you, Grandma."
+  return 
+}
